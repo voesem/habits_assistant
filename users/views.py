1e45f59cfa68e679f1b386c67eaed2b906018588
Vladimir Semikov <voesem@ya.ru>
2023-11-16 22:39:59 +0300

    Созданы представления для регистрации и авторизации пользователя

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer, MyTokenObtainPairSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
