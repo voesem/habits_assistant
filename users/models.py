5807a5e19b44c3716117255f47eeacce903a0188
Vladimir Semikov <voesem@ya.ru>
2023-11-16 22:36:24 +0300

    Исправлено наследование модели User

--- users/models.py
+++ users/models.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 NULLABLE = {'null': True, 'blank': True}
 
 
-class User(models.Model):
+class User(AbstractUser):
     """
     Модель пользователя
 
