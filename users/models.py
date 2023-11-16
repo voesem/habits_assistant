7b83235f8b336bfa35ad72ad735c7b2ba8c79c39
Vladimir Semikov <voesem@ya.ru>
2023-11-16 20:25:46 +0300

    Создана модель User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+
+NULLABLE = {'null': True, 'blank': True}
+
+
+class User(models.Model):
+    """
+    Модель пользователя
+
+    Поля:
+    email: адрес электронной почты.
+    name: имя.
+    is_active: признак активности.
+
+    """
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+    name = models.CharField(max_length=200)
+    is_active = models.BooleanField(default=True, verbose_name='признак активности')
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        """
+        :return: Строковое представление пользователя по адресу электронной почты.
+        """
+        return f'{self.email}'
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
