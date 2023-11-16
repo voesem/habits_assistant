267ecd62ab4ad2551076410e6e5c376b8d660fc4
Vladimir Semikov <voesem@ya.ru>
2023-11-16 22:41:03 +0300

    Созданы представления для модели Habit по механизму CRUD

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
+
+class HabitCreateView(generics.CreateAPIView):
+    """ Создание привычки """
+
+    serializer_class = HabitSerializer
+
+    def perform_create(self, serializer):
+        """ При создании привычки устанавливается связь с текущим пользователем. """
+        serializer.save(user=self.request.user)
+
+
+class HabitListView(generics.ListAPIView):
+    """ Список привычек """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitRetrieveView(generics.RetrieveAPIView):
+    """ Просмотр привычки """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitUpdateView(generics.UpdateAPIView):
+    """ Редактирование привычки """
+
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+
+
+class HabitDestroyView(generics.DestroyAPIView):
+    """ Удаление привычки """
+
+    queryset = Habit.objects.all()
