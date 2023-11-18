72e815c97186d993ac68478a0ee02e4e27332d92
Vladimir Semikov <voesem@ya.ru>
2023-11-18 11:51:24 +0300

    Прописаны маршруты для регистрации, просмотра профиля, редактирования, удаления и просмотра списка пользователей

--- users/urls.py
+++ users/urls.py
@@ -1,17 +1,21 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework.authtoken.views import obtain_auth_token
 
 from users.apps import UsersConfig
-from users.views import UserViewSet, MyTokenObtainPairView
+from users.views import MyTokenObtainPairView, UserCreateView, UserListView, UserRetrieveView, UserUpdateView, \
+    UserDestroyView
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
-router.register(r'users', UserViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
+    path('create/', UserCreateView.as_view(), name='create_user'),
+    path('', UserListView.as_view(), name='users_list'),
+    path('<int:pk>/', UserRetrieveView.as_view(), name='view_user'),
+    path('update/<int:pk>/', UserUpdateView.as_view(), name='update_user'),
+    path('delete/<int:pk>/', UserDestroyView.as_view(), name='delete_user'),
+
     path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('token/', obtain_auth_token, name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
