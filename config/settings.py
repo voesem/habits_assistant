4d8d654f54e0c34f65613e04a30771eadefd4270
Vladimir Semikov <voesem@ya.ru>
2023-11-18 19:56:41 +0300

    Внесены настройки пакета celery

--- config/settings.py
+++ config/settings.py
@@ -145,3 +145,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
 }
+
+CELERY_BROKER_URL = "redis://127.0.0.1:6379/0"
+CELERY_RESULT_BACKEND = "redis://127.0.0.1:6379/0"
