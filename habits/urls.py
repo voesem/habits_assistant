a0347e969f3f7cd80ef5d0830fb8f5795e9b234f
Vladimir Semikov <voesem@ya.ru>
2023-11-18 11:47:59 +0300

    Создан адрес для публичных привычек

--- habits/urls.py
+++ habits/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path
 
 from habits.apps import HabitsConfig
-from habits.views import HabitCreateView, HabitListView, HabitRetrieveView, HabitUpdateView, HabitDestroyView
+from habits.views import HabitCreateView, HabitListView, HabitRetrieveView, HabitUpdateView, HabitDestroyView, \
+    PublicHabitListView
 
 app_name = HabitsConfig.name
 
 urlpatterns = [
     path('habits/create/', HabitCreateView.as_view(), name='create_habit'),
     path('habits/', HabitListView.as_view(), name='habits_list'),
+    path('habits/public/', PublicHabitListView.as_view(), name='public_habits_list'),
     path('habits/<int:pk>/', HabitRetrieveView.as_view(), name='view_habit'),
     path('habits/update/<int:pk>/', HabitUpdateView.as_view(), name='update_habit'),
     path('habits/delete/<int:pk>/', HabitDestroyView.as_view(), name='delete_habit'),
