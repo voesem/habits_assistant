f2316e6e83bf05529aec3f91488f6524a0b45cbc
Vladimir Semikov <voesem@ya.ru>
2023-11-18 11:48:58 +0300

    Представления для работы с пользователем переделаны на generics

--- users/views.py
+++ users/views.py
@@ -1,12 +1,40 @@
-from rest_framework import viewsets
+from rest_framework import generics
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from users.models import User
 from users.serializers import UserSerializer, MyTokenObtainPairSerializer
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserCreateView(generics.CreateAPIView):
+    """ Представление регистрации пользователя """
+
     serializer_class = UserSerializer
+
+
+class UserListView(generics.ListAPIView):
+    """ Список пользователей """
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserRetrieveView(generics.RetrieveAPIView):
+    """ Просмотр профиля пользователя """
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    """ Редактирование профиля пользователя """
+
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyView(generics.DestroyAPIView):
+    """ Удаление пользователя """
+
     queryset = User.objects.all()
 
 
